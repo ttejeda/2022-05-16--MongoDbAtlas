d878ce0d0efe0b8f6ff51ee4f2b403631fb5d91d
ttejeda <ttejeda@ucol.mx>
2022-05-13 08:51:18 -0500

    implemented MongoDB Atlas connection

--- src/server.js
+++ src/server.js
@@ -11,3 +11,18 @@ let PORT = process.env.PORT || 3000; //Se queda abierto en el puerto que le desi
 app.listen(PORT, () => {
     console.log('escuchando en el puerto 3000'); //Mensaje para asegurarse de que se levantó el servidor correctamente
 });
+
+const mongoose = require('mongoose'); //Exportar módulo de mongoose
+mongoose.connect(
+`mongodb+srv://ttejeda:jOIJJnQ6gEILygAu@cluster0.uy9zi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`, //Conección al cluster de MongoDB Atlas
+{
+useNewUrlParser: true,
+useUnifiedTopology: true
+}
+);
+
+const db = mongoose.connection; //Establece conexión con la base de datos
+db.on("error", console.error.bind(console, "connection error: ")); //Mensaje de error si no se conecta adecuadamente
+db.once("open", function () {
+console.log("Connected successfully"); //Mensaje de éxito de conexión
+});
